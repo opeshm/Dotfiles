2cbe89826e35ad99817796204d62c3f0a9af7466
Mario Dominguez <opeshm@gmail.com>
2023-04-22 08:47:29 +0200

    Add nvim autocomplete configuration

---
+++ .config/nvim/after/plugin/lspconfig.lua
@@ -0,0 +1,17 @@
+local coq = require('coq')
+local lspconfig = require('lspconfig')
+
+vim.g.coq_settings = {
+  auto_start = true,
+  clients = {
+    tabnine = {
+      enabled = true,
+    }
+  }
+}
+
+vim.cmd("COQnow --shut-up")
+
+lspconfig.pyright.setup {coq.lsp_ensure_capabilities()}
+lspconfig.tsserver.setup {coq.lsp_ensure_capabilities()}
+

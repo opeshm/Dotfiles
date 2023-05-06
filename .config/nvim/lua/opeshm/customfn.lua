03ce1a02d910a9ac670ddc4f1ebe8cca6cbb3b66
Mario Dominguez <opeshm@gmail.com>
2023-05-06 14:08:24 +0200

    NVim config. Add new custom function to show ColorColumn

---
+++ .config/nvim/lua/opeshm/customfn.lua
@@ -0,0 +1,4 @@
+function showColorColumn ()
+    vim.cmd("set colorcolumn=80")
+    vim.cmd("highlight ColorColumn ctermbg=235 guibg=#cc00ff")
+end
--- .config/nvim/lua/opeshm/init.lua
+++ .config/nvim/lua/opeshm/init.lua
@@ -1,3 +1,4 @@
 require("opeshm.set")
 require("opeshm.remap")
 require("opeshm.packer")
+require("opeshm.customfn")

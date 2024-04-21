35fd9081cf4af660a3f22e5946b4f979e833cda2
Mario Domínguez <opeshm@gmail.com>
2024-04-21 14:14:32 +0200

    Update i3 startup and wm-startup script

--- .config/wm-startup.sh
+++ .config/wm-startup.sh
@@ -8,30 +8,31 @@ setxkbmap -option caps:swapescape
 echo "Checkin num monitors..." >> /tmp/wm-startup.log
 MONITOR_COUNT=$(xrandr -q | grep " connected" | cut -d ' ' -f1 | wc -l)
 
-# Init Monitors
-if [[ "${MONITOR_COUNT}" == "2" ]]; then
-    if [ -f ~/.screenlayout/Dual-laptop-right.sh ]; then
-        . ~/.screenlayout/Dual-laptop-right.sh
-    fi
-fi
+# Init Monitors configuration
+case "${HOSTNAME}" in
+  "nzxt")
+    ~/.config/screenlayout/nzxt-default.sh
+    ;;
+esac
 
 # Polybar
 case "${HOSTNAME}" in
-    "nzxt")
-        . ~/.config/polybar/docky-nzxtarch/launch.sh
-        ;;
-    "asusarch")
-        . ~/.config/polybar/docky-asus/launch.sh
-        ;;
-    "archalpha")
-        echo "launching archalpha" >> /tmp/wm-startup.log
-        . ~/.config/polybar/docky-alpha15/launch.sh
-        if [[ "${MONITOR_COUNT}" == "2" ]]; then
-            . ~/.config/polybar/docky-alpha15-extended/launch.sh
-        fi
-        ;;
+  "nzxt")
+    ~/.config/polybar-nzxt/launch.sh
+    ;;
+  "asusarch")
+    ~/.config/polybar/docky-asus/launch.sh
+    ;;
+  "archalpha")
+    echo "launching archalpha" >> /tmp/wm-startup.log
+    ~/.config/polybar/docky-alpha15/launch.sh
+    if [[ "${MONITOR_COUNT}" == "2" ]]; then
+      ~/.config/polybar/docky-alpha15-extended/launch.sh
+    fi
+    ;;
 esac
 
+nitrogen --restore
 
 nm-applet &
 
@@ -41,20 +42,20 @@ sysmodmap=/etc/X11/xinit/.Xmodmap
 # merge in defaults and keymaps
 
 if [ -f $sysresources ]; then
-    xrdb -merge $sysresources
+  xrdb -merge $sysresources
 fi
 
 if [ -f $sysmodmap ]; then
-    xmodmap $sysmodmap
+  xmodmap $sysmodmap
 fi
 
 # start some nice programs
 
 if [ -d /etc/X11/xinit/xinitrc.d ] ; then
- for f in /etc/X11/xinit/xinitrc.d/?*.sh ; do
-  [ -x "$f" ] && . "$f"
- done
- unset f
+  for f in /etc/X11/xinit/xinitrc.d/?*.sh ; do
+    [ -x "$f" ] && . "$f"
+  done
+  unset f
 fi
 
 # Make sure this is before the 'exec' command or it won't be sourced.
